f4737c969424a7329a1863cfdde69ddd8336e2f7
andrewrjones <andrewjones86@googlemail.com>
2012-01-04 22:13:17 +0000

    initial implimentation

--- test/uk/co/arjones/ant/task/SitemapTest.java
+++ test/uk/co/arjones/ant/task/SitemapTest.java
@@ -1,6 +1,8 @@
 package uk.co.arjones.ant.task;
 
 import org.apache.tools.ant.BuildFileTest;
+import com.redfin.sitemapgenerator.SitemapValidator;
+import java.io.File;
 
 public class SitemapTest extends BuildFileTest {
 
@@ -12,8 +14,30 @@ public class SitemapTest extends BuildFileTest {
         // initialize Ant
         configureProject("build.xml");
     }
+    
+    public void testWithoutURL() {
+        expectBuildException("use.without.url", "Fail requested.");
+    }
+    
+    public void testWithoutDestDir() {
+        expectBuildException("use.without.destdir", "Fail requested.");
+    }
 
     public void testWithoutFiles() {
-        expectBuildException("use.without", "Fail requested.");
+        expectBuildException("use.without.files", "Fail requested.");
+    }
+    
+    public void testDefault() {
+        executeTarget("use.default");
+
+        // check sitemap is valid
+        File sitemap = new File(getProjectDir()+"/test-resources/sitemap.xml");
+        try {
+            SitemapValidator.validateWebSitemap(sitemap);
+        } catch( Exception e ){
+            fail("exception thrown when validating sitemap");
+        }
+        
+        sitemap.deleteOnExit();
     }
 }
